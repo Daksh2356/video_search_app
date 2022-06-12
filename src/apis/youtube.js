5a1870e1bca86e9e6b65818e304e5cdd7473240a
Daksh Makhija <91017744+Daksh2356@users.noreply.github.com>
2022-06-13 02:13:48 +0530

    Update youtube.js

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_KEY = "AIzaSyAqf-7glqrYzUR3spytKDoXAiTN0dntVo8";
+const API_KEY ="AIzaSyA4ejtvs6HSy7S9a9Cp6t9j4V_JJkY7XSQ";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3/",
