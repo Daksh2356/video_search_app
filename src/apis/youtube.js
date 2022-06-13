b84632cdd5d53860ebb4fe6a4428ed2b121290d4
Daksh2356 <dakshmakhija@gmail.com>
2022-06-13 13:18:23 +0530

    Changes

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,9 +1,5 @@
 import axios from "axios";
-<<<<<<< HEAD
 const API_KEY = "AIzaSyBy36z0vnHoowz5V4Dc1_EtWX8kZ1H8nHU";
-=======
-const API_KEY ="AIzaSyA4ejtvs6HSy7S9a9Cp6t9j4V_JJkY7XSQ";
->>>>>>> 5a1870e1bca86e9e6b65818e304e5cdd7473240a
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3/",
